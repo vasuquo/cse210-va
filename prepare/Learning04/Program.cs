bdd40e962200218e87303131c896d2d7948becec
vasuquo <veasuquo@gmail.com>
2024-03-19 14:25:11 +0100

    learning04 commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Victor Asuquo", "Calculus");    
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(" ");
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez","Fractions", "7.3","8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine(" ");
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters","European History","The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
